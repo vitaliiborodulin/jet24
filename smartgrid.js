e33d33ac16137f94f8ba02f47328e4a61c05edeb
vitaliiborodulin <vitaliiborodulin@yandex.ru>
2026-07-31 15:18:18 +0300

    first commit

---
+++ smartgrid.js
@@ -0,0 +1,23 @@
+module.exports = {
+	columns: 12,
+	offset: "20px",
+	container: {
+		maxWidth: "1320px",
+		fields: "20px" // fields must be >= offset / 2
+	},
+	breakPoints: {
+		// xl: {
+		// 	width: "1200px",
+		// },
+		// lg: {
+		// 	width: "992px",
+		// },
+		md: {
+			width: "768px"
+		},
+		// xs: {
+		// 	width: "576px",
+		// 	// fields: "10px"
+		// },
+	}
+}
---
+++ src/js/scripts.js
@@ -0,0 +1,12 @@
+$(function() {
+
+    
+    console.log(1);
+    
+
+        
+        
+});
+
+
+   
