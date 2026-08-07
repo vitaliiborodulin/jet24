2a43ae4daaba22c800fc2ecd07ed1bc4b3eda877
vitaliiborodulin <vitaliiborodulin@yandex.ru>
2026-08-07 18:05:42 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -135,4 +135,39 @@ $(function() {
          closeExisting : true
      });
 
+     $('.form__select').niceSelect();
+
+     $(".cal").flatpickr({
+          mode: "range",
+          minDate: "today",
+          dateFormat: "d.m",
+          // disableMobile: "true"
+     });
+
+     $(document).on('click', '.plus', function () {
+          console.log(1);
+          const $form = $(this).closest('.form__item');
+          const $input = $form.find('.form__input');
+
+          $input.val(parseInt($input.val(), 10) + 1);
+     });
+
+     $(document).on('click', '.minus', function () {
+          const $form = $(this).closest('.form__item');
+          const $input = $form.find('.form__input');
+
+          let value = parseInt($input.val(), 10);
+
+          if (value > parseInt($input.attr('min'), 10)) {
+               $input.val(value - 1);
+          }
+     });
+
+
+     document.querySelectorAll("form").forEach(form => {
+          form.addEventListener("submit", event => {
+               event.preventDefault();
+          });
+     });
+
 });
