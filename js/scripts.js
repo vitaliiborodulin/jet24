9f70cb30aefee36602469bb4dd26edc75684d548
vitaliiborodulin <vitaliiborodulin@yandex.ru>
2026-08-12 11:24:47 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -74,6 +74,38 @@ $(function() {
          }
        });
      });
+     // burger
+     
+     const burger = $('.burger');
+     const menu = $('.header');
+     
+     burger.on('click', function(e) {
+         e.preventDefault();
+         burger.toggleClass('burger-close');
+         menu.toggleClass('open');
+         $('html, body').toggleClass('overflow');
+     });
+     
+     $(document).keyup(function(e) {
+         if (e.key === "Escape" || e.keyCode === 27) {
+             menu.removeClass('open');
+             burger.removeClass('burger-close');
+             $('html, body').removeClass('overflow');
+         }
+     });
+     
+     $('body').on('click', '.header__menu a[href*="#"]', function(e) {
+         var fixed_offset = 50;
+     
+         $('html,body').stop().animate({
+             scrollTop: $(this.hash).offset().top - fixed_offset
+         }, 300);
+         e.preventDefault();
+         burger.toggleClass('burger-close');
+         menu.toggleClass('open');
+         $('html, body').removeClass('overflow');
+         // $('html').toggleClass('scroll-disabled');
+     });
      if ($(window).width() <= 768) {
      
          $('.features__item').on('click', function(){
@@ -216,6 +248,38 @@ $(function() {
          initFleetTableSlider();
          initStepsSlider();
      });
+     // form select
+     
+     $('.form__select').niceSelect();
+     
+     // form calendar
+     
+     $(".cal").flatpickr({
+         mode: "range",
+         minDate: "today",
+         dateFormat: "d.m",
+         // disableMobile: "true"
+     });
+     
+     // form passengers
+     
+     $(document).on('click', '.plus', function () {
+         const $form = $(this).closest('.form__item');
+         const $input = $form.find('.form__input');
+     
+         $input.val(parseInt($input.val(), 10) + 1);
+     });
+     
+     $(document).on('click', '.minus', function () {
+         const $form = $(this).closest('.form__item');
+         const $input = $form.find('.form__input');
+     
+         let value = parseInt($input.val(), 10);
+     
+         if (value > parseInt($input.attr('min'), 10)) {
+             $input.val(value - 1);
+         }
+     });
      $('.popup-link').fancybox({
          // thumbs : { autoStart:true },
          toolbar: "auto",
@@ -252,32 +316,9 @@ $(function() {
          }, 300)
      });
 
-     $('.form__select').niceSelect();
+     
 
-     $(".cal").flatpickr({
-          mode: "range",
-          minDate: "today",
-          dateFormat: "d.m",
-          // disableMobile: "true"
-     });
-
-     $(document).on('click', '.plus', function () {
-          const $form = $(this).closest('.form__item');
-          const $input = $form.find('.form__input');
-
-          $input.val(parseInt($input.val(), 10) + 1);
-     });
-
-     $(document).on('click', '.minus', function () {
-          const $form = $(this).closest('.form__item');
-          const $input = $form.find('.form__input');
-
-          let value = parseInt($input.val(), 10);
-
-          if (value > parseInt($input.attr('min'), 10)) {
-               $input.val(value - 1);
-          }
-     });
+     
 
 
      // document.querySelectorAll("form").forEach(form => {
